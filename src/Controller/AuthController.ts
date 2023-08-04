030ba2968047447dba00f5be971de5ba4b2d403b
ArchSharp <raphael.tunji@yahoo.com>
2023-08-04 19:58:52 +0100

    minor changes

--- src/Controller/AuthController.ts
+++ src/Controller/AuthController.ts
@@ -18,22 +18,26 @@ import { ISignUp } from "../Models/ISignUp";
 const stdTab = "Student";
 const dbId = "Email";
 export const signup = async (req: Request, res: Response): Promise<void> => {
+  const payload: ISignUp = req.body;
   try {
-    const payload: ISignUp = req.body;
     const userId = payload.Email;
-    console.log(userId);
+    // console.log(userId);
     var isUserExist = await FirstOrDefault(stdTab, dbId, userId);
     if (isUserExist != null) {
       const error = Message(400, UserIsExist);
+      console.log("seen-1, ", error);
       res.status(400).json(error);
     } else {
+      console.log("seen-2");
       const response = await AddToDB(stdTab, payload);
+      console.log("seen-3, ", response);
       const success = Message(200, CreateSuccess, response);
       res.status(200).json(success);
     }
     // console.log(payload);
   } catch (error) {
     const err = Message(500, InternalError);
+    console.log("see payload: ", payload);
     res.status(500).json(err);
   }
 };
