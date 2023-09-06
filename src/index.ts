3a6880a2aaaab216063adab5859ddaebe9e41190
ArchSharp <raphael.tunji@yahoo.com>
2023-09-07 00:12:39 +0100

    http

--- src/index.ts
+++ src/index.ts
@@ -87,8 +87,8 @@ if (cluster.isPrimary) {
 
       // const ip = "127.0.0.1";
       //   const ip = "192.168.137.1";
-      // const port = 80;
-      const port = 443;
+      const port = 80;
+      // const port = 443;
       // const port = 3000;
 
       server.listen(
